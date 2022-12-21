2a33ab6d363eb74d7bc6702c50020897eedcf093
Egor Zhuravlev <zhuravlevegor@gmail.com>
2022-12-21 22:14:02 +0300

    Example2 modified

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -11,14 +11,14 @@ int M = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число N - ");
 int N = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Сумма = {SumNumbers(M,N)}");
+Console.WriteLine($"Сумма = {SumNumbers(M, N)}");
 
 
 
 int SumNumbers(int M, int N)
 {
     if (M == 0) return (N * (N + 1)) / 2;
-    else if (N == 0) return (M * (M + 1)) /2;
+    else if (N == 0) return (M * (M + 1)) / 2;
     else if (M == N) return M;
     else if (M < N) return N + SumNumbers(M, N - 1);
     else return N + SumNumbers(M, N + 1);
